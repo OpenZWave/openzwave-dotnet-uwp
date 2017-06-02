04438bbd879186d280dc2a72f6dda012b99356e2
Morten Nielsen <no@spam.thx>
2017-06-01 21:01:36 -0700

    Change output name of .NET dll to match UWP

--- nuget/BuildPackage.cmd
+++ nuget/BuildPackage.cmd
@@ -14,11 +14,9 @@ XCOPY ..\src\Output\OpenZWaveUWP\x86\Release\OpenZWave.xml package\lib\uap10.0\ 
 MD package\build\uap10.0\
 COPY OpenZWave.uap10.0.targets package\build\uap10.0\%PackageName%.targets /Y
 REM .NET4.5.2...
-XCOPY ..\src\Output\OpenZWaveDotNet\x86\Release\OpenZWaveDotNet.dll package\lib\net452\ /Y
-XCOPY ..\src\Output\OpenZWaveDotNet\x86\Release\OpenZWaveDotNet.xml package\lib\net452\ /Y
+XCOPY ..\src\Output\OpenZWaveDotNet\x86\Release\OpenZWave.dll package\lib\net452\ /Y
+XCOPY ..\src\Output\OpenZWaveDotNet\x86\Release\OpenZWave.xml package\lib\net452\ /Y
 MD package\build\net452\
 COPY OpenZWave.net452.targets package\build\net452\%PackageName%.targets /Y
-COPY ..\src\Output\OpenZWaveUWP\x86\Release\OpenZWave.xml package\lib\net452\OpenZWaveDotNet.xml /Y
-
 nuget pack package\OpenZWave.nuspec -Properties packageid=%PackageName%;version=%Version%
 PAUSE
